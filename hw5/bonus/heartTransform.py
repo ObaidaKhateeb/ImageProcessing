c58bf5a039385327c6106150473b5798f8f0fa16
ObaidaKhateeb <okhati01@campus.haifa.ac.il>
2025-02-28 16:24:34 +0200

    updated

--- hw5/bonus/heartTransform.py
+++ hw5/bonus/heartTransform.py
@@ -9,20 +9,20 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 
 def parametric_x(t):
-	# Your code goes here
+    return 14.5 * np.sin(t)** 3
 
 def parametric_y(t):
-	# Your code goes here
+    return 0.5 * np.cos(4*t) +2 * np.cos(3*t) + 4* np.cos(2*t) - 13* np.cos(t)
 
 '''
 This is an example implementation for if we did a circle's transform.
 
 def parametric_x(t):
-	return np.cos(t)
+    return np.cos(t)
 
 def parametric_y(t):
-	return np.sin(t)
-	
+    return np.sin(t)
+    
 '''
 
 def find_hough_shape(image, edge_image, r_min, r_max, bin_threshold):
@@ -40,21 +40,22 @@ def find_hough_shape(image, edge_image, r_min, r_max, bin_threshold):
 	shape_candidates = []
 	for r in rs:
 		for theta in thetas:
-			# Your code goes here
+			shape_candidates.append((r, theta))
 
 
 	# Hough Accumulator. We are using defaultdict instead of standard dict as this will initialize for keys which are not already present in the dictionary instead of throwing an exception.
 	accumulator = defaultdict(int)
 
-	edge_points = # Extract points that were detected as an edge in edge_image
+	edge_points = np.argwhere(edge_image> 0) # Extract points that were detected as an edge in edge_image
 	for point in edge_points:
 		y, x = point
 		for r, theta in shape_candidates:
-			# Your code goes here
-			# Don't forget - the points should be integers!
+			x_center = int(x - r * cos_thetas[theta // 2])
+			y_center = int(y - r * sin_thetas[theta // 2])
 			
 			#vote for current candidate
-			accumulator[(x_center, y_center, r)] += 1
+			if x_center >= 0 and x_center < img_width and y_center >= 0 and y_center < img_height:
+				accumulator[(x_center, y_center, r)] += 1
 
 	# Output image with detected lines drawn
 	output_img = image.copy()
@@ -62,11 +63,12 @@ def find_hough_shape(image, edge_image, r_min, r_max, bin_threshold):
 	out_shapes = []
 
 	# Sort the accumulator based on the votes for the candidate circles 
+	num_thetas = len(thetas)
 	for candidate_shape, votes in sorted(accumulator.items(), key=lambda i: -i[1]):
 		x, y, r = candidate_shape
 		current_vote_percentage = votes / num_thetas
-		
-		# Your code goes here
+		if current_vote_percentage > bin_threshold:
+			out_shapes.append((x, y, r, current_vote_percentage))
 		# If the current_vote_percentage is larger than bin_threshold then we want that shape. append x,y,r and the precentage to the out_shapes.
 
 
@@ -82,12 +84,12 @@ def find_hough_shape(image, edge_image, r_min, r_max, bin_threshold):
 
 
 	# Draw shortlisted hearts on the output image
-	for x, y, r, v in out_shapes:	
-	
+	for x, y, r, v in out_shapes:   
+
 		# We found heart at (x,y) with radius r. 
 		# Generate all the x1 and y1 points of that heart.
-		x1 = # Your code goes here
-		y1 = # Your code goes here
+		x1 = x + r * parametric_x(np.linspace(0, 2 * np.pi, 100))
+		y1 = x + r * parametric_x(np.linspace(0, 2 * np.pi, 100))
 
 		colors = [
 			(255, 0, 0),  # 'b' (blue)
@@ -111,29 +113,29 @@ def find_hough_shape(image, edge_image, r_min, r_max, bin_threshold):
 	return output_img
 
 
-IMAGE_NAME = "simple"
+IMAGE_NAME = "med"
 
 image = cv2.imread(f'{IMAGE_NAME}.jpg')
 edge_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 min_edge_threshold, max_edge_threshold = 100, 200
-edge_image = # Apply edge detector with min_edge_threshold, max_edge_threshold
+edge_image = cv2.Canny(edge_image, min_edge_threshold, max_edge_threshold) # Apply edge detector with min_edge_threshold, max_edge_threshold
 
-r_min = 1
-r_max = 4
+r_min = 2.1 #1
+r_max = 5.1 #4
 
-bin_threshold = 0.1
+bin_threshold = 0.29 #0.1
 
 if edge_image is not None:
-	
-	print ("Attempting to detect Hough hearts...")
-	results_img = find_hough_shape(image, edge_image, r_min, r_max, bin_threshold)
-	
-	if results_img is not None:
-		plt.imshow(cv2.cvtColor(results_img, cv2.COLOR_BGR2RGB))
-		plt.show()
-		cv2.imwrite(f'{IMAGE_NAME}_detected.jpg', results_img)
-	else:
-		print ("Error in input image!")
+    
+    print ("Attempting to detect Hough hearts...")
+    results_img = find_hough_shape(image, edge_image, r_min, r_max, bin_threshold)
+    
+    if results_img is not None:
+        plt.imshow(cv2.cvtColor(results_img, cv2.COLOR_BGR2RGB))
+        plt.show()
+        cv2.imwrite(f'{IMAGE_NAME}_detected.jpg', results_img)
+    else:
+        print ("Error in input image!")
 
-	print ("Hough hearts detection complete!")
+    print ("Hough hearts detection complete!")
